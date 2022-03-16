a848df852f669dcf554ff7d75053c233777ef2b9
8327 <8327>
2022-03-16 20:11:35 +0000

    switch to vlc

--- playout.py
+++ playout.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 # playout would be a little daemon/wrapper that listens to commands (play, pause, file, url,
-# volumne, etc.) somewhere(?) and then instructs mpg321 what to do. This should prevent two 
+# volumne, etc.) somewhere(?) and then instructs vlc what to do. This should prevent two 
 # audio streams beeing played back at the same time, etc.
 
-from mpyg321.mpyg321 import MPyg321Player
+import time
+import vlc
 
-player = MPyg321Player()
+media = vlc.MediaPlayer('https://orf-live.ors-shoutcast.at/oe1-q2a')
+media.play()
 
-player.play_song("sound.mp3")
+time.sleep(10)
 
