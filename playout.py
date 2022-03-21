be7a82d887f7aead482ce59d34cbb4dcf9d3baaf
8327 <8327>
2022-03-21 13:57:35 +0000

    use bus instead of unix sockets

--- playout.py
+++ playout.py
@@ -1,79 +1,58 @@
 #!/usr/bin/env python3
 
-# playout would be a little daemon/wrapper that listens to commands (play, pause, file, url,
-# volumne, etc.) somewhere(?) and then instructs vlc what to do. This should prevent two 
-# audio streams beeing played back at the same time, etc.
+"""
+playout is a little wrapper around VLC (via python-vlc) that runs as a daemon 
+and interprets a simplistic set of audio commands (play, pause, vol, ...).
+
+"""
 
 import os
 import socket
 import time
 import vlc
+import sys
 
-# test using:
-# > nc -U playout.control.socket
-# > play https://orf-live.ors-shoutcast.at/oe1-q2a
-
+# todo
+sys.path.append('../bus')
+import bus
 
 media = None
 
 class Playout:
-
-    vlc_instance = None
-    player = None
-    media = None
-
     def __init__(self):
         self.vlc_instance = vlc.Instance()
         self.player = self.vlc_instance.media_player_new()
-
     def play(self, media):
         self.media = self.vlc_instance.media_new(media)
         self.player.set_media(self.media)
         self.player.play()
-
     def pause(self):
         self.player.pause()
-
     def stop(self):
         self.player.stop()
 
 
-
-def prep():
-    server_address = './playout.control.socket'
-    if os.path.exists(server_address):
-        os.unlink(server_address)
-    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    sock.bind(server_address)
-    sock.listen(1)
-    return sock
-
-def execute(playout, command, params):
+def execute(command, params):
     if command == 'play':
         url = params[0]
         playout.play(url)
+    elif command == 'pause':
+        playout.pause()
+    elif command == 'stop':
+        playout.stop()
+    elif command == 'vol':
+        playout.audio_set_volume(params[0])
 
+def receive(message):
+    split = message.decode("utf-8".rstrip()).split()
+    if len(message) > 0:
+        execute(split[0], split[1:])
+    else:
+        print("empty message")
 
-def main():
-    playout = Playout()
-    sock = prep()
-    # currently only handles one connection at a time
-    while True:
-        connection, client = sock.accept()
-        try:
-            while True:
-                data = connection.recv(1024)
-                if not data:
-                    break;
-                line = data.decode("utf-8".rstrip())
-                print(line)
-                split = line.split()
-                execute(playout, split[0], split[1:])
-        finally:
-            print("conn close")
-            connection.close()
-
-if __name__ == "__main__":
-    main()
+playout = Playout()
+mybus = bus.Bus(receive)
+while True:
+        time.sleep(1)
 
 
